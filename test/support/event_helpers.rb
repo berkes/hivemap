e8ee501b702eff5e4810961dbda90c5fc9a91f9e
Bèr Kessels <ber@berk.es>
2018-03-27 17:18:26 +0200

    Add a test to determine that the proposed-nodes projector works.

--- test/support/event_helpers.rb
+++ test/support/event_helpers.rb
@@ -4,4 +4,11 @@ module EventHelpers
   def last_event(aggregate_id)
     HiveMap.event_store.get_events_for_aggregate_id(aggregate_id).last
   end
+
+  def projector_process_event(aggregate_id)
+    ## TODO: refactor to avoid harcoding the projectors here.
+    projector = HiveMap::Projections::ProposedNodes::Projector.new
+    projector.setup
+    projector.process(last_event(aggregate_id))
+  end
 end
